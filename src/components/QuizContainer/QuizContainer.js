a4cfbe71dc0a4f6b6541001c8d7beaf07b3fe766
ahsan sayeed <ahsansayeedsaad@gmail.com>
2022-10-11 19:33:57 +0100

    toast added

--- src/components/QuizContainer/QuizContainer.js
+++ src/components/QuizContainer/QuizContainer.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import QuizQuestions from "../QuizQuestions/QuizQuestions";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const QuizContainer = () => {
   const { data } = useLoaderData();
-  console.log(data.questions);
+  const notify = (e) => toast(e);
+
   return (
-    <div>
+    <div className="my-5">
       <h1 className="text-center">Quiz of {data.name}</h1>
-      {data.questions.map((value,index) => (
+      {data.questions.map((value, index) => (
         <QuizQuestions
           key={value.id}
           questions={value.question}
           correctAns={value.correctAnswer}
           options={value.options}
-          id={value.id}
           index={index}
+          notify={notify}
         ></QuizQuestions>
       ))}
+      <ToastContainer />
     </div>
   );
 };
--- src/components/QuizQuestions/QuizQuestions.js
+++ src/components/QuizQuestions/QuizQuestions.js
@@ -1,19 +1,29 @@
 import React from "react";
 
-const QuizQuestions = ({ questions, correctAns, options, id,index }) => {
+const QuizQuestions = ({ questions, correctAns, options, index, notify }) => {
   return (
-    <div className="container my-5 mx-auto px-5 py-4 d-flex flex-column" style={{border:'2px solid red'}}>
+    <div
+      className="container my-5 mx-auto px-5 py-4 d-flex flex-column rounded shadow p-3 mb-5 rounded"
+      style={{ backgroundColor: "#F9FEFD", position: "relative" }}
+    >
       <div>
-       <h4>Quiz {index+1}: {questions}</h4>
-     </div>
+        <div className="col-11">
+          <h4>
+            Quiz {index + 1}: {questions}
+          </h4>
+        </div>
+        <div style={{ position: "absolute", top: "10px", right: "10px" }}>
+          <button onClick={() => notify(correctAns)}>Click</button>
+        </div>
+      </div>
       <div>
         <form className="row mx-auto">
-          {options.map((value,idx) => (
-            <div key={idx} className='col-sm-6'>
-            <label htmlFor={value} >
+          {options.map((value, idx) => (
+            <div key={idx} className="col-sm-6 d-flex align-items-center g-4">
               <input type="radio" id={value} name="option" value={value} />
-              {value}
-            </label>
+              <label htmlFor={value} className="ms-4">
+                {value}
+              </label>
             </div>
           ))}
           <br />
